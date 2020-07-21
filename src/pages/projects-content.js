4d62ad61ef30b5e403318b420956bb5d743428ec
Tino <tinomuzambi@gmail.com>
2020-07-21 06:36:29 +0200

    Adds projects.tinomuzambi project.

--- src/pages/projects-content.js
+++ src/pages/projects-content.js
@@ -1,4 +1,34 @@
 const projects = [
+    {
+        name: "projects.tinomuzambi",
+        shortname: "PT",
+		title: "Projects.TinoMuzambi",
+		content: [
+			`Llorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            fringilla tristique vulputate. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+			`Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+			`Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        ],
+        link : "https://projects.tinomuzambi.com",
+        github: "https://github.com/TinoMuzambi/Projects.TinoMuzambi",
+        keywords: [
+            "react", "html", "css", "npm", "node.js", "bootstrap", "javascript",
+        ],
+	},
 	{
         name: "whatsapp-analyser",
         shortname: "WA",
