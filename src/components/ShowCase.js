066783eb691a572fadc3f7649360d75d80135a48
Tino <tinomuzambi@gmail.com>
2020-09-05 14:42:16 +0200

    Adds new project.

--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -1,23 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import projects from "../pages/projects-content";
 import { Link } from "react-router-dom";
-
 
 class ShowCase extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			name: props.match.params.name
-		}
+			name: props.match.params.name,
+		};
 	}
 
 	componentDidUpdate() {
-		window.scrollTo(0, 0)
+		window.scrollTo(0, 0);
 	}
 
 	render() {
 		const name = this.state.name;
-		const project = projects.find(project => project.name === name);
+		const project = projects.find((project) => project.name === name);
+		document.title = `${project.title} | Projects.TinoMuzambi`;
 		return (
 			<>
 				<h1 className="project-title">{project.title}</h1>
@@ -28,28 +28,39 @@ class ShowCase extends Component {
 						</div>
 						<div className="projects-desc-pars">
 							{project.content.map((paragraph, key) => (
-								<p key={key} className="project-desc-text">{paragraph}</p>
+								<p key={key} className="project-desc-text">
+									{paragraph}
+								</p>
 							))}
 						</div>
 					</div>
 					<div className="project-footer">
-						<p className={project.link === "" ? "link-hide" : ""}>Link: <a target="_blank"
-																					   rel="noopener noreferrer"
-																					   href={project.link}>{project.link.substring(0, 5) === "https" ? project.link.substring(8) : project.link.substring(7)}</a>
+						<p className={project.link === "" ? "link-hide" : ""}>
+							Link:{" "}
+							<a target="_blank" rel="noopener noreferrer" href={project.link}>
+								{project.link.substring(0, 5) === "https"
+									? project.link.substring(8)
+									: project.link.substring(7)}
+							</a>
 						</p>
-						<p className={project.github === "" ? "link-hide" : ""}>GitHub: <a target="_blank"
-																						   rel="noopener noreferrer"
-																						   href={project.github}>{project.github.substring(19)}</a>
+						<p className={project.github === "" ? "link-hide" : ""}>
+							GitHub:{" "}
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href={project.github}
+							>
+								{project.github.substring(19)}
+							</a>
 						</p>
 						<ul className="tag">
-							{
-								project.keywords.map((keyword, key) => (
-									<li className="tags" key={key}>
-										<Link to={`/tags/${keyword}`}>
-											<p className="tag-text">{keyword}</p>
-										</Link>
-									</li>
-								))}
+							{project.keywords.map((keyword, key) => (
+								<li className="tags" key={key}>
+									<Link to={`/tags/${keyword}`}>
+										<p className="tag-text">{keyword}</p>
+									</Link>
+								</li>
+							))}
 						</ul>
 					</div>
 				</div>
