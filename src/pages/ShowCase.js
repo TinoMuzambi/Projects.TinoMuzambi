90cd1e53794a79cb8b63f96234decea1df228232
Tino <tinomuzambi@gmail.com>
2020-11-03 20:58:11 +0200

    Fixes minor issues in css.

--- src/pages/ShowCase.js
+++ src/pages/ShowCase.js
@@ -21,47 +21,53 @@ class ShowCase extends Component {
 		return (
 			<>
 				<h1 className="project-title">{project.title}</h1>
-				<div className="wrapper" data-aos="flip-up" data-aos-delay="200">
-					<div className="project-desc">
-						<div className="project-image">
-							<p className="project-image-text">{project.shortname}</p>
+				<div className="project">
+					<div className="wrapper" data-aos="flip-up" data-aos-delay="200">
+						<div className="project-desc">
+							<div className="project-image">
+								<p className="project-image-text">{project.shortname}</p>
+							</div>
+							<div className="projects-desc-pars">
+								{project.content.map((paragraph, key) => (
+									<p key={key} className="project-desc-text">
+										{paragraph}
+									</p>
+								))}
+							</div>
 						</div>
-						<div className="projects-desc-pars">
-							{project.content.map((paragraph, key) => (
-								<p key={key} className="project-desc-text">
-									{paragraph}
-								</p>
-							))}
+						<div className="project-footer">
+							<p className={project.link === "" ? "link-hide" : ""}>
+								Link:{" "}
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href={project.link}
+								>
+									{project.link.substring(0, 5) === "https"
+										? project.link.substring(8)
+										: project.link.substring(7)}
+								</a>
+							</p>
+							<p className={project.github === "" ? "link-hide" : ""}>
+								GitHub:{" "}
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href={project.github}
+								>
+									{project.github.substring(19)}
+								</a>
+							</p>
+							<ul className="tag">
+								{project.keywords.map((keyword, key) => (
+									<li className="tags" key={key}>
+										<Link to={`/tags/${keyword}`}>
+											<p className="tag-text">{keyword}</p>
+										</Link>
+									</li>
+								))}
+							</ul>
 						</div>
-					</div>
-					<div className="project-footer">
-						<p className={project.link === "" ? "link-hide" : ""}>
-							Link:{" "}
-							<a target="_blank" rel="noopener noreferrer" href={project.link}>
-								{project.link.substring(0, 5) === "https"
-									? project.link.substring(8)
-									: project.link.substring(7)}
-							</a>
-						</p>
-						<p className={project.github === "" ? "link-hide" : ""}>
-							GitHub:{" "}
-							<a
-								target="_blank"
-								rel="noopener noreferrer"
-								href={project.github}
-							>
-								{project.github.substring(19)}
-							</a>
-						</p>
-						<ul className="tag">
-							{project.keywords.map((keyword, key) => (
-								<li className="tags" key={key}>
-									<Link to={`/tags/${keyword}`}>
-										<p className="tag-text">{keyword}</p>
-									</Link>
-								</li>
-							))}
-						</ul>
 					</div>
 				</div>
 			</>
