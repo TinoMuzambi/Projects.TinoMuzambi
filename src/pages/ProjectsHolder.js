f9fa165c524fcba1111af31e6f341e43ef31ef2b
Tino <tinomuzambi@gmail.com>
2021-02-04 23:08:22 +0200

    Makes project holder a functional component.

--- src/pages/ProjectsHolder.js
+++ src/pages/ProjectsHolder.js
@@ -1,42 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Project from "../components/Project";
 import projects from "../data/projects-content";
 import SearchProjects from "../components/SearchProjects";
 
-class ProjectsHolder extends Component {
-	constructor() {
-		super();
-		this.state = {
-			queryText: "",
-		};
-		this.searchProj = this.searchProj.bind(this);
-	}
+const ProjectsHolder = () => {
+	const [queryText, setQueryText] = useState("");
 
-	searchProj(query) {
-		this.setState({ queryText: query });
-	}
+	const searchProj = (query) => {
+		setQueryText(query);
+	};
 
-	render() {
-		const filteredProjects = projects.filter((eachItem) => {
-			return (
-				eachItem["title"]
-					.toLowerCase()
-					.includes(this.state.queryText.toLowerCase()) ||
-				eachItem["keywords"].join().includes(this.state.queryText.toLowerCase())
-			);
-		});
+	const filteredProjects = projects.filter((eachItem) => {
 		return (
-			<>
-				<div className="headline">
-					<h1 className="project-title">Projects</h1>
-					<SearchProjects searchProj={this.searchProj} />
-				</div>
-				<div className="projects">
-					<Project projects={filteredProjects} />
-				</div>
-			</>
+			eachItem["title"].toLowerCase().includes(queryText.toLowerCase()) ||
+			eachItem["keywords"].join().includes(queryText.toLowerCase())
 		);
-	}
-}
+	});
+	return (
+		<>
+			<div className="headline">
+				<h1 className="project-title">Projects</h1>
+				<SearchProjects searchProj={searchProj} />
+			</div>
+			<div className="projects">
+				<Project projects={filteredProjects} />
+			</div>
+		</>
+	);
+};
 
 export default ProjectsHolder;
