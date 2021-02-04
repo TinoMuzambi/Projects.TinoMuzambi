039d43b9e6472968c6c9bc5a7541782463263a67
Tino <tinomuzambi@gmail.com>
2021-02-04 23:04:47 +0200

    Makes app a functional component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ProjectsHolder from "./pages/ProjectsHolder";
@@ -10,42 +10,38 @@ import AOS from "aos";
 import "./App.css";
 import { Helmet } from "react-helmet";
 
-class App extends Component {
-	componentDidMount() {
+const App = () => {
+	useEffect(() => {
 		AOS.init();
-	}
+	}, []);
 
-	render() {
-		return (
-			<>
-				<Router>
-					<NavBar />
-					<div className="page-body">
-						<Switch>
-							<div className="outer">
-								<Route
-									path="/"
-									exact
-									render={() => (
-										<>
-											<Helmet>
-												<title>Projects.TinoMuzambi</title>
-											</Helmet>
-											<ProjectsHolder />
-										</>
-									)}
-								/>
-								<Route path="/showcase/:name" component={ShowCase} />
-								<Route path="/tags/:name" component={Tags} />
-							</div>
-							<Route component={NotFoundPage} />
-						</Switch>
+	return (
+		<Router>
+			<NavBar />
+			<div className="page-body">
+				<Switch>
+					<div className="outer">
+						<Route
+							path="/"
+							exact
+							render={() => (
+								<>
+									<Helmet>
+										<title>Projects.TinoMuzambi</title>
+									</Helmet>
+									<ProjectsHolder />
+								</>
+							)}
+						/>
+						<Route path="/showcase/:name" component={ShowCase} />
+						<Route path="/tags/:name" component={Tags} />
 					</div>
-					<Footer />
-				</Router>
-			</>
-		);
-	}
-}
+					<Route component={NotFoundPage} />
+				</Switch>
+			</div>
+			<Footer />
+		</Router>
+	);
+};
 
 export default App;
