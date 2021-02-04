899864fb2f7541bc517afb26600bb1f3875c61a5
Tino <tinomuzambi@gmail.com>
2021-02-04 23:05:41 +0200

    Removes unnecessary fragment in not found page.

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
-    <>
-        <div className="not-found">
-            <h1 className="four04-title">404</h1>
-            <p className="four04-body">Looks like I'm still working on that project.</p>
-            <p className="four04-body2">Let's talk about it on <a href="https://www.linkedin.com/in/tinomuzambi/">LinkedIn.</a></p>
-            <p className="four04-body2">or</p>
-            <Link to="/">
-                <p className="four04-redirect">Return to home</p>
-            </Link>
-        </div>
-    </>
+	<div className="not-found">
+		<h1 className="four04-title">404</h1>
+		<p className="four04-body">Looks like I'm still working on that project.</p>
+		<p className="four04-body2">
+			Let's talk about it on{" "}
+			<a href="https://www.linkedin.com/in/tinomuzambi/">LinkedIn.</a>
+		</p>
+		<p className="four04-body2">or</p>
+		<Link to="/">
+			<p className="four04-redirect">Return to home</p>
+		</Link>
+	</div>
 );
 
 export default NotFoundPage;
