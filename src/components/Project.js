33c275e9eb85fbb50e4eee0686090f97831823c5
Tino <tinomuzambi@gmail.com>
2021-02-13 13:25:17 +0200

    Adds message for no search results.

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom";
 
 const Project = ({ projects }) => (
 	<>
-		{projects.map((project, key) => (
-			<div className="wrapper" key={key}>
-				<Link to={`/showcase/${project.name}`}>
-					<h2 className="projects-title">{project.title}</h2>
-					<div className="project-desc">
-						<div className="project-image">
-							<p className="project-image-text">{project.shortname}</p>
+		{projects.length > 0 ? (
+			projects.map((project, key) => (
+				<div className="wrapper" key={key}>
+					<Link to={`/showcase/${project.name}`}>
+						<h2 className="projects-title">{project.title}</h2>
+						<div className="project-desc">
+							<div className="project-image">
+								<p className="project-image-text">{project.shortname}</p>
+							</div>
+							<p className="project-desc-text">{project.content[0]}</p>
 						</div>
-						<p className="project-desc-text">{project.content[0]}</p>
-					</div>
-				</Link>
-			</div>
-		))}
+					</Link>
+				</div>
+			))
+		) : (
+			<h1>There are no projects that match that search term.</h1>
+		)}
 	</>
 );
 
