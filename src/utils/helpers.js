15922a9b1b71a36039915f0bbabbd5de205907cb
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-16 19:25:48 +0200

    Adds filters.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,30 @@
 export const applyFilters = (tags) => {
-	return tags;
+	return tags.filter(
+		(i) =>
+			i !== "jsx" &&
+			i !== "content management system" &&
+			i !== "teachable machine" &&
+			i !== "clash of clans" &&
+			i !== "server-side-rendering" &&
+			i !== "custom components" &&
+			i !== "responsive" &&
+			i !== "nextjs" &&
+			i !== "nodejs" &&
+			i !== "socketio" &&
+			i !== "ml5js" &&
+			i !== "p5js" &&
+			i !== "google" &&
+			i !== "dark mode" &&
+			i !== "godaddy" &&
+			i !== "video" &&
+			i !== "table tennis" &&
+			i !== "sports" &&
+			i !== "obs studio" &&
+			i !== "seo" &&
+			i !== "go" &&
+			i !== "mathematics" &&
+			i !== "email" &&
+			i !== "uct" &&
+			i !== "classification"
+	);
 };
