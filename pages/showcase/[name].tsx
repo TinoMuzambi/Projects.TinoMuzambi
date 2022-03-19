7b55190fe8537d70516060b33b9cd2d5d66287b2
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-19 17:55:41 +0200

    Move project to state.

--- pages/showcase/[name].tsx
+++ pages/showcase/[name].tsx
@@ -10,27 +10,28 @@ const ShowCase: React.FC = (): JSX.Element => {
 	const router = useRouter();
 	const { projects } = useContext(AppContext);
 	const [name] = useState(router.query.name);
+	const [project, setProject] = useState<Project>();
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
 
-	const project: Project = projects.find(
-		(project) => project.name === name
-	) as Project;
+	useEffect(() => {
+		setProject(projects.find((project) => project?.name === name) as Project);
+	}, []);
 
 	return (
 		<>
-			<Meta title={`${project.title} | Projects.TinoMuzambi`} />
-			<h1 className="project-title">{project.title}</h1>
+			<Meta title={`${project?.title} | Projects.TinoMuzambi`} />
+			<h1 className="project-title">{project?.title}</h1>
 			<div className="project">
 				<div className="wrapper" data-aos="flip-up" data-aos-delay="200">
 					<div className="project-desc">
 						<div className="project-image">
-							<p className="project-image-text">{project.shortname}</p>
+							<p className="project-image-text">{project?.shortname}</p>
 						</div>
 						<div className="projects-desc-pars">
-							{project.content.map((paragraph: string, key: number) => (
+							{project?.content.map((paragraph: string, key: number) => (
 								<p key={key} className="project-desc-text">
 									{paragraph}
 								</p>
@@ -38,26 +39,26 @@ const ShowCase: React.FC = (): JSX.Element => {
 						</div>
 					</div>
 					<div className="project-footer">
-						<p className={project.link === "" ? "link-hide" : ""}>
+						<p className={project?.link === "" ? "link-hide" : ""}>
 							Link:{" "}
-							<a target="_blank" rel="noopener noreferrer" href={project.link}>
-								{project.link.substring(0, 5) === "https"
-									? project.link.substring(8)
-									: project.link.substring(7)}
+							<a target="_blank" rel="noopener noreferrer" href={project?.link}>
+								{project?.link.substring(0, 5) === "https"
+									? project?.link.substring(8)
+									: project?.link.substring(7)}
 							</a>
 						</p>
-						<p className={project.github === "" ? "link-hide" : ""}>
+						<p className={project?.github === "" ? "link-hide" : ""}>
 							GitHub:{" "}
 							<a
 								target="_blank"
 								rel="noopener noreferrer"
-								href={project.github}
+								href={project?.github}
 							>
-								{project.github.substring(19)}
+								{project?.github.substring(19)}
 							</a>
 						</p>
 						<ul className="tag">
-							{project.keywords.map((keyword: string, key: number) => (
+							{project?.keywords.map((keyword: string, key: number) => (
 								<li className="tags" key={key}>
 									<Link href={`/tags/${keyword}`}>
 										<a>
