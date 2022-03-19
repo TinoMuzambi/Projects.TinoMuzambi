5393191b486ab1ef940d00eb2d7341e494401c99
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-19 18:01:31 +0200

    Uses context.

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useRouter } from "next/router";
+import { AppContext } from "../../context/AppContext";
 
-const OpenSearch: React.FC = ({ setQueryText }: any): JSX.Element => {
+const OpenSearch: React.FC = (): JSX.Element => {
 	const router = useRouter();
+	const { setQueryText } = useContext(AppContext);
 
 	useEffect(() => {
 		const query = router.pathname.substring(8);
-		setQueryText(query);
+		if (setQueryText) setQueryText(query);
 		router.push("/", {
 			query: {
 				fromOpenSearch: true,
