5f9a42fb92d7cdddf9274084c461f2312f86cc53
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-20 12:22:40 +0200

    Adds header.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,25 +6,27 @@ const NavBar: React.FC = (): JSX.Element => {
 	const { setQueryText } = useContext(AppContext);
 
 	return (
-		<nav className="nav">
-			<Link href="/">
-				<a
-					onClick={() => {
-						if (setQueryText) setQueryText("");
-					}}
-				>
-					Projects.TinoMuzambi
-				</a>
-			</Link>
-			<div className="secondary">
-				<Link href="/tags">
-					<a>Tags</a>
+		<header>
+			<nav className="nav">
+				<Link href="/">
+					<a
+						onClick={() => {
+							if (setQueryText) setQueryText("");
+						}}
+					>
+						Projects.TinoMuzambi
+					</a>
 				</Link>
-				<Link href="/featured">
-					<a>Featured</a>
-				</Link>
-			</div>
-		</nav>
+				<div className="secondary">
+					<Link href="/tags">
+						<a>Tags</a>
+					</Link>
+					<Link href="/featured">
+						<a>Featured</a>
+					</Link>
+				</div>
+			</nav>
+		</header>
 	);
 };
 
