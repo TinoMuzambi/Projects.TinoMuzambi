445c995521af56a70d6a866b1dc89b74d982335f
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-20 12:52:10 +0200

    Styles tags.

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -3,16 +3,16 @@ import { HomeTagsProps } from "../interfaces";
 
 const Tags: React.FC<HomeTagsProps> = ({ links }): JSX.Element => {
 	return (
-		<>
+		<div className="tags">
 			<h1>Tags</h1>
 			<div className="quick-links" data-aos="fade-up">
 				{links.map((i, key) => (
 					<Link key={key} href={`/tags/${i}`}>
-						<a className="tags">{i}</a>
+						<a className="tag">{i}</a>
 					</Link>
 				))}
 			</div>
-		</>
+		</div>
 	);
 };
 
