6e7d081b62b212e85f7c4270f88b7bf27539cf64
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-21 19:16:43 +0200

    Removes fragment.

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,21 +4,19 @@ import { motion } from "framer-motion";
 import { ProjectProps } from "../interfaces";
 
 const Project: React.FC<ProjectProps> = ({ project }): JSX.Element => (
-	<>
-		<motion.div className={`card${project.featured ? " featured" : ""}`} layout>
-			<Link href={`/showcase/${project.name}`}>
-				<a>
-					<h2 className="title">{project.title}</h2>
-					<div className="body">
-						<div className="image">
-							<p className="text">{project.shortname}</p>
-						</div>
-						<p className="description">{project.content[0]}</p>
+	<motion.div className={`card${project.featured ? " featured" : ""}`} layout>
+		<Link href={`/showcase/${project.name}`}>
+			<a>
+				<h2 className="title">{project.title}</h2>
+				<div className="body">
+					<div className="image">
+						<p className="text">{project.shortname}</p>
 					</div>
-				</a>
-			</Link>
-		</motion.div>
-	</>
+					<p className="description">{project.content[0]}</p>
+				</div>
+			</a>
+		</Link>
+	</motion.div>
 );
 
 export default Project;
