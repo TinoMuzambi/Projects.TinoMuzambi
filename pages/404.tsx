de1a45b7f69956dbc142a5e361112d4aa82f3ff1
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-21 19:15:31 +0200

    Adds meta.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
+import Meta from "../components/Meta";
 
 const NotFoundPage = () => (
-	<main className="not-found">
-		<h1 className="four04-title">404</h1>
-		<p className="four04-body">
-			Looks like I&apos;m still working on that project.
-		</p>
-		<p className="four04-body2">
-			Let&apos;s talk about it on{" "}
-			<a href="https://www.linkedin.com/in/tinomuzambi/">LinkedIn.</a>
-		</p>
-		<p className="four04-body2">or</p>
-		<Link href="/">
-			<a className="four04-redirect">Return to home</a>
-		</Link>
-	</main>
+	<>
+		<Meta title="Page Not Found | Projects.TinoMuzambi" />
+		<main className="not-found">
+			<h1 className="four04-title">404</h1>
+			<p className="four04-body">
+				Looks like I&apos;m still working on that project.
+			</p>
+			<p className="four04-body2">
+				Let&apos;s talk about it on{" "}
+				<a href="https://www.linkedin.com/in/tinomuzambi/">LinkedIn.</a>
+			</p>
+			<p className="four04-body2">or</p>
+			<Link href="/">
+				<a className="four04-redirect">Return to home</a>
+			</Link>
+		</main>
+	</>
 );
 
 export default NotFoundPage;
