364dba44009bc6009f37ef9d6be1cfe57d5a8028
TinoMuzambi <tinomuzambi@gmail.com>
2025-01-09 18:37:15 +0200

    Programmatically get year.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,10 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer: React.FC = (): JSX.Element => (
 	<footer>
-		<h4>Made with React. Copyright © Tino Muzambi 2020 - 2022</h4>
+		<h4>
+			Made with React. Copyright © Tino Muzambi 2020 -{" "}
+			{new Date().getFullYear()}
+		</h4>
 
 		<div className="social">
 			<a
